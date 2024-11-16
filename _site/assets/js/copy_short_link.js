be26c3efa9c8d5551925bd8345c1e3768b758e64
cnzixn <cnzixn@gmail.com>
2024-11-17 00:25:03 +0800

    Update gh-pages at 2024-11-17 00:25:03

--- _site/assets/js/copy_short_link.js
+++
@@ -1,9 +0,0 @@
-// <script>
-  // 复制链接
-  function copyShortLink() {
-    var copyText = document.getElementById('shortLink');
-    copyText.select();
-    document.execCommand('copy');
-    // alert("链接已复制到剪贴板！");
-  }
-// </script>
