4e8f52d917b1138f87a8ae568e48f97c412b86a8
cnzixn <cnzixn@gmail.com>
2024-11-17 00:17:58 +0800

    Deploy gh-pages

---
+++ bny/_site/assets/js/copy_short_link.js
@@ -0,0 +1,9 @@
+// <script>
+  // 复制链接
+  function copyShortLink() {
+    var copyText = document.getElementById('shortLink');
+    copyText.select();
+    document.execCommand('copy');
+    // alert("链接已复制到剪贴板！");
+  }
+// </script>
---
+++ bny/_site/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# cd /sdcard/acode/otfo.cc/
+# bash start.sh
+
+# jekyll build
+# jekyll clean
+jekyll serve
