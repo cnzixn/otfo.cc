fb6973e2c97602bca56cab2de449b25c97532a2c
cnzixn <cnzixn@gmail.com>
2024-11-16 23:09:25 +0800

    Update main

---
+++ deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# cd /sdcard/acode/otfo.cc/
+# bash deploy.sh
+
+# 网页子目录
+dir="bny"
+
+# 设置远端仓库
+# git remote set-url origin https://github.com/cnzixn/otfo.cc.git
+
+# 新建一个本地仓库，并设置远端仓库
+# git init
+# git remote add origin https://github.com/cnzixn/otfo.cc.git
+
+# 切换到主分支，构建站点并提交更改
+git checkout main
+jekyll build
+git add .
+git commit -m "Update main"
+git push origin main --force
+
+# 切换到 gh-pages 分支，清空内容
+git checkout gh-pages
+git rm -rf .
+
+# 将 _site 文件夹内容复制到 gh-pages 分支根目录
+git checkout main -- CNAME
+git checkout main -- .gitignore
+git checkout main -- _site
+git checkout main -- search.json
+cp -r ./_site ./"$dir"
+cp -r ./"$dir"/s .
+# cp ./"$dir"/index.html .
+cp ./"$dir"/404.html .
+
+# 提交并推送到 gh-pages 分支
+git add .
+git reset deploy.sh
+git reset start.sh
+git commit -m "Deploy gh-pages"
+git push origin gh-pages --force
+
+# 返回到主分支
+git checkout main
+rm -rf ./"$dir"
+git add .
+git commit -m "Update gh-pages"
+
